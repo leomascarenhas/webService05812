79204458029945800507f2526af1c616b2508434
Leonardo Mascarenhas <leopmasc@gmail.com>
2023-02-06 16:13:52 -0500

    Implement spring security

---
+++ customers-api/src/main/java/ca/vanier/customersapi/security/ApplicationSecurityConfiguration.java
@@ -0,0 +1,57 @@
+package ca.vanier.customersapi.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class ApplicationSecurityConfiguration {
+
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public ApplicationSecurityConfiguration(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf().disable()
+                .headers().frameOptions().disable();
+
+        http
+                .authorizeHttpRequests()
+                .requestMatchers("/", "index")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+
+                .and()
+                .httpBasic()
+
+                .and()
+                .formLogin();
+
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.withUsername("student")
+                .password(passwordEncoder.encode("password"))
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
+
+}
